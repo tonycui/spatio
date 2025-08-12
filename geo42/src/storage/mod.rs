d2b091c00a2d1876641eb0e01186580657add956
cui.tao <cui.tao@tomoloyalty.com>
2025-08-12 12:35:25 +0800

    rename storage_new.rs to storage.rs

--- geo42/src/storage/mod.rs
+++ geo42/src/storage/mod.rs
@@ -1,7 +1,7 @@
-pub mod storage_new;
+pub mod storage;
 pub mod geo_utils;
 pub mod geometry_utils;
 
-pub use storage_new::{GeoDatabase};
+pub use storage::{GeoDatabase};
 pub use geo_utils::{string_to_data_id};
 pub use geometry_utils::{geojson_to_geometry, geometries_intersect, geometries_distance, geometries_haversine_distance};
